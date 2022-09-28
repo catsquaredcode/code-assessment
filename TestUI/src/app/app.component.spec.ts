b04b4190eee11de180e0137bf2ccd2ace6e2b610
Ricardo <rlpza.94@gmail.com>
2022-09-28 10:29:14 -0500

    CAT2 Assessment: TestUI

--- TestUI/src/app/app.component.spec.ts
+++ TestUI/src/app/app.component.spec.ts
@@ -1,35 +1,105 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { Client, WeatherForecast } from './weatherapp.swagger';
+import { of, throwError } from 'rxjs';
+
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let client: Client;
+  let http: HttpClient;
+  let httpController: HttpTestingController;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      declarations: [AppComponent],
+      providers: [Client],
     }).compileComponents();
+
+    client = TestBed.inject(Client);
+    http = TestBed.inject(HttpClient);
+    httpController = TestBed.inject(HttpTestingController);
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it('should create AppComponent', () => {
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'TestUI'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('TestUI');
+  it(`should create an instance of 'client' service`, () => {
+    expect(client).toBeDefined();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should call component.getWeather method in constructor', function () {
+    const spy = spyOn(AppComponent.prototype, 'getWeather').and.stub();
+    new AppComponent(client);
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  it('component title should be "Weather Forecast"', function () {
+    expect(component.title).toEqual('Weather Forecast');
+  });
+
+  it('component.getWeather should return a list on success and update component.weatherData', async(() => {
+    const response: WeatherForecast[] = [
+      {
+        date: new Date('2022-09-28T00:00:00'),
+        temperatureC: 28,
+        temperatureF: 82,
+        summary: 'Balmy',
+      },
+      {
+        date: new Date('2022-09-29T00:00:00'),
+        temperatureC: 10,
+        temperatureF: 49,
+        summary: 'Cool',
+      },
+    ];
+    spyOn(Client.prototype, 'unauthenticated').and.returnValue(of(response));
+    component.getWeather();
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('TestUI app is running!');
+    expect(component.weatherData).toEqual(response);
+  }));
+
+  it('component.getWeather should trigger component.handleError on error', async(() => {
+    spyOn(Client.prototype, 'unauthenticated').and.returnValue(
+      throwError('500')
+    );
+    const error = spyOn(component, 'handleError').and.stub();
+    component.getWeather();
+    fixture.detectChanges();
+    expect(error).toHaveBeenCalledTimes(1);
+  }));
+
+  it('#setColor should return a color based on component.colors', () => {
+    const testCases = [
+      { value: 'Freezing', color: 'cyan' },
+      { value: 'Bracing', color: 'cyan' },
+      { value: 'Chilly', color: 'cyan' },
+      { value: 'Mild', color: 'green' },
+      { value: 'Balmy', color: 'green' },
+      { value: 'Cool', color: 'green' },
+      { value: 'Warm', color: 'orange' },
+      { value: 'Hot', color: 'orange' },
+      { value: 'Sweltering', color: 'red' },
+      { value: 'Scorching', color: 'red' },
+      { value: 'Other', color: '' },
+      { value: undefined, color: '' },
+    ];
+
+    testCases.forEach((test) => {
+      let color = component.setColor(test.value);
+      expect(color).toBe(test.color);
+    });
   });
 });
--- TestUI/src/app/app.component.ts
+++ TestUI/src/app/app.component.ts
@@ -1,18 +1,27 @@
-import {Component} from '@angular/core';
-import {Client, WeatherForecast} from "./weatherapp.swagger";
+import { Component } from '@angular/core';
+import { Client, WeatherForecast } from './weatherapp.swagger';
 
+interface Color {
+  color: string;
+  items: string[];
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [Client]
+  providers: [Client],
 })
 export class AppComponent {
+  title: string = 'Weather Forecast';
+  colors: Color[] = [
+    { color: 'cyan', items: ['freezing', 'bracing', 'chilly'] },
+    { color: 'green', items: ['mild', 'balmy', 'cool'] },
+    { color: 'orange', items: ['warm', 'hot'] },
+    { color: 'red', items: ['sweltering', 'scorching'] },
+  ];
   weatherData: WeatherForecast[] = [];
 
-  constructor(
-    private client: Client
-  ) {
+  constructor(private client: Client) {
     this.getWeather();
   }
 
@@ -29,10 +38,24 @@ export class AppComponent {
       },
       next: (data: WeatherForecast[]) => {
         this.weatherData = data;
-      }
-    })
+      },
+    });
   }
 
+  /**
+   * Set Summary Color
+   *
+   * @description returns a string that defines a color based on summary
+   */
+  setColor(summary?: string): string {
+    if (summary!) {
+      const temp = this.colors.filter(
+        (e: Color) => e.items.indexOf(summary.toLowerCase()) > -1
+      );
+      return temp.length ? temp[0].color : '';
+    }
+    return '';
+  }
 
   /**
    * Dummy Error Handler
