2f9ed26f1c9b47b860d7af1c42ea35bdaace8c49
IsaacMGW <126019192+IsaacMGW@users.noreply.github.com>
2023-02-22 15:16:55 -0600

    Update IWeatherDatabase.cs

--- TestAPI/Database/IWeatherDatabase.cs
+++ TestAPI/Database/IWeatherDatabase.cs
@@ -1,15 +1,20 @@
-﻿using System.Threading;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using TestAPI.Models;
-
 namespace TestAPI.Database
 {
-  public interface IWeatherDatabase
-  {
-    DbSet<Summary> Summaries { get; set; }
-    DbSet<Forecast> Forecasts { get; set; }
-    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
-    void Migrate();
-  }
+    public interface IWeatherDatabase
+    {
+        DbSet<Summary> Summaries { get; set; }
+        DbSet<Forecast> Forecasts { get; set; }
+        Task<Dictionary<DateTime, Forecast>> GetForecastsByDate(DateTime startDate, DateTime endDate, CancellationToken token);
+        Task<List<Summary>> GetSummariesAsAList(CancellationToken token);
+        void AddToForecasts(Forecast forecast);
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+        void Migrate();
+    }
 }
