72e9efd1e4faa03ad54bf7cb5ca5b043de37121e
Dario Giordano <hello@dariogiordano.com>
2024-03-13 22:36:29 +0100

    added logic to color summary text

--- TestUI/src/app/app.component.ts
+++ TestUI/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {Client, WeatherForecast} from "./weatherapp.swagger";
+import {Client, WeatherForecast, WeatherForecastPresentation, summaryCat} from "./weatherapp.swagger";
 
 @Component({
   selector: 'app-root',
@@ -8,7 +8,7 @@ import {Client, WeatherForecast} from "./weatherapp.swagger";
   providers: [Client]
 })
 export class AppComponent {
-  weatherData: WeatherForecast[] = [];
+  weatherData: WeatherForecastPresentation[] = [];
   title:string="TestUI"
   constructor(
     private client: Client
@@ -28,11 +28,43 @@ export class AppComponent {
         this.handleError(error);
       },
       next: (data: WeatherForecast[]) => {
-        this.weatherData = data;
+        this.weatherData = data.map(wf=>{return this.WFMapToPresentation(wf) });
       }
     })
   }
 
+  /**
+   * Converts a weatherForecast object into a presentation representation by adding a CSS class based on the summary.
+   * @param {WeatherForecast} wf - Weather forecast object to be converted.
+   * @returns {WeatherForecastPresentation} - Presentation representation of the weather forecast with an added CSS class.
+   * @description This function takes a WeatherForecast object as input and determines the appropriate CSS class based on the weather summary. It then returns a copy of the WeatherForecast object with the CSS class added.
+   */
+  WFMapToPresentation(wf:WeatherForecast):WeatherForecastPresentation{
+    let cssClass:string
+      switch (wf.summary){
+        case summaryCat.FREEZING:
+        case summaryCat.BRACING:
+        case summaryCat.CHILLY:
+          cssClass="cyan"
+          break;
+        case summaryCat.MILD:
+        case summaryCat.BALMY:
+        case summaryCat.COOL:
+          cssClass="Green"
+          break;
+        case summaryCat.WARM:
+        case summaryCat.HOT:
+          cssClass="orange"
+          break;
+        case summaryCat.SCORCHING:
+        case summaryCat.SWELTERING:
+          cssClass="red"
+          break;
+        default:
+          cssClass="neutral"
+      }
+      return {...wf,...{cssClass}}
+    }
 
   /**
    * Dummy Error Handler
