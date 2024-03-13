6f92812323015169efb2bcc9f1505dab1f1e7610
Thomas Fozzi <43391847+tfozzi@users.noreply.github.com>
2024-03-13 01:47:12 +0100

    unit test coverage

--- TestUI/src/app/app.component.spec.ts
+++ TestUI/src/app/app.component.spec.ts
@@ -2,8 +2,23 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AppComponent } from './app.component';
+import { of, throwError } from 'rxjs';
+
+const weatherData = [
+  {
+    summary: 'Hot', 
+    temperatureC: 38, 
+    temperatureF: 100, 
+    dateFormatted: '2024-01-01'
+  }
+];
 
 describe('AppComponent', () => {
+
+  let fixture = TestBed.createComponent(AppComponent);
+  let app = fixture.componentInstance;
+
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -17,21 +32,43 @@ describe('AppComponent', () => {
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'TestUI'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app.title).toEqual('TestUI');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('TestUI app is running!');
+    const rendered = fixture.nativeElement as HTMLElement;
+    expect(rendered.querySelector('.content span')?.textContent).toContain('TestUI app is running!');
   });
+
+  describe('getWeather', () => {
+
+    it('should handle complete', () => {
+      const completeSpy = spyOn( app, 'getWeather').and.callThrough();
+      app.getWeather();
+      expect(completeSpy).toHaveBeenCalled();
+    }); 
+  
+    it('should handle error', () => {
+      const error = 'Error message';
+      spyOn(app, 'handleError');
+      app.getWeather();
+      expect(app.handleError).toHaveBeenCalledWith(error);
+    }); 
+  
+    it('should handle next', () => {
+      app.getWeather();
+      expect(app.weatherData).toEqual(weatherData);
+    }); 
+  
+  }); 
+
 });
+
+
+
+
