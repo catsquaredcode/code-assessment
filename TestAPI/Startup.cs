376d6a10d25c3649f9b8d17181c6740ddb1cb5f4
Giuseppe Dinatale <giuseppe.dinatale@external.eni.com>
2024-03-16 20:11:17 +0100

    Fix(be): change connection string to run locally

--- TestAPI/Startup.cs
+++ TestAPI/Startup.cs
@@ -12,57 +12,57 @@ using TestAPI.Services;
 
 namespace TestAPI
 {
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-      Configuration = configuration;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<WeatherDatabase>(options => options.UseSqlServer("Data Source=DESKTOP-AQJAF5O\\MSSQLSERVERNEW;Initial Catalog=Weather;Integrated Security=True;"));
+            services.AddScoped<IWeatherDatabase>(sp => sp.GetRequiredService<WeatherDatabase>());
+            services.AddControllers();
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "TestAPI", Version = "v1" }); });
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters ??= new TokenValidationParameters();
+                options.TokenValidationParameters.ValidIssuer = Configuration["JWT:Issuer"];
+                options.TokenValidationParameters.ValidateAudience = false;
+            });
+
+            services.AddTransient<IWeatherForecastService, WeatherForecastService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TestAPI v1"));
+
+                app.UseCors(options =>
+                    options.WithOrigins("http://localhost:4200")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                );
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
     }
-
-    public IConfiguration Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.AddDbContext<WeatherDatabase>(options => options.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=Weather;Integrated Security=True;"));
-      services.AddScoped<IWeatherDatabase>(sp => sp.GetRequiredService<WeatherDatabase>());
-      services.AddControllers();
-      services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "TestAPI", Version = "v1"}); });
-
-      services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-      {
-        options.TokenValidationParameters ??= new TokenValidationParameters();
-        options.TokenValidationParameters.ValidIssuer = Configuration["JWT:Issuer"];
-        options.TokenValidationParameters.ValidateAudience = false;
-      });
-
-      services.AddTransient<IWeatherForecastService, WeatherForecastService>();
-    }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-        app.UseSwagger();
-        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TestAPI v1"));
-
-        app.UseCors(options =>
-            options.WithOrigins("http://localhost:4200")
-            .AllowAnyMethod()
-            .AllowAnyHeader()
-        );
-      }
-
-      app.UseHttpsRedirection();
-      
-      app.UseRouting();
-
-      app.UseAuthentication();
-      app.UseAuthorization();
-
-      app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
-    }
-  }
 }
