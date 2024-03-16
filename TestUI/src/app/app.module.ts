ac534c1eaae298e7dccb61ff6e94adddacae4c4a
Giuseppe Dinatale <giuseppe.dinatale@external.eni.com>
2024-03-16 20:25:06 +0100

    Feat(app-module): add injection token for browser locale to format date

--- TestUI/src/app/app.module.ts
+++ TestUI/src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
+import { InjectionToken, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {API_BASE_URL, Client} from "./weatherapp.swagger";
-import {HttpClient, HttpClientModule} from "@angular/common/http";
-import {environment} from "../environments/environment";
+import { API_BASE_URL, Client } from "./weatherapp.swagger";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { environment } from "../environments/environment";
+
+export const BROWSER_LOCALE = new InjectionToken<string>('BROWSER_LOCALE');
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import {environment} from "../environments/environment";
   providers: [
     Client,
     HttpClient,
-    {provide: API_BASE_URL, useValue: environment.url},
+    { provide: API_BASE_URL, useValue: environment.url },
+    { provide: BROWSER_LOCALE, useValue: navigator.language } // Use the browser's language
   ],
   bootstrap: [AppComponent]
 })
